437b6540ff5992cffe1cefac5b1e8758f81614ac
pudov maxim <maksimpudov6@gmail.com>
2024-12-05 09:35:10 +0300

    Update task1.c

--- 3824Б1ПР3/15 Пудов Максим Владимирович/task1.c
+++ 3824Б1ПР3/15 Пудов Максим Владимирович/task1.c
@@ -3,5 +3,12 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+	int sum = a + b;
+	if (sum > INT_MAX) {
+		return INT_MAX;
+	}
+	if (sum < INT_MIN) {
+		return INT_MIN;
+	}
+	return 0;
 }
